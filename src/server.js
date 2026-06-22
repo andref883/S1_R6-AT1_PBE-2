08545f6d46cb8920f7210a843b707549fdfb36c8
ANDRÉ FERNANDES ANTUNES DA SILVA <andre.silva328@senaisp.edu.br>
2026-06-22 10:52:46 -0300

    quarto commit

--- src/server.js
+++ src/server.js
@@ -3,19 +3,25 @@ import express from 'express';
 import routes from './routes/routes.js';
 import { initializeDatabase } from './configs/Database.js';
 
-
 const app = express();
 
-
+// Middleware para receber JSON e formulários
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Disponibiliza a pasta uploads publicamente
+app.use('/uploads', express.static('uploads'));
+
+// Rotas da aplicação
 app.use('/', routes);
 
-
-initializeDatabase().then(() => {
-    app.listen(process.env.SERVER_PORT, () => {
-        console.log(`Servidor rodando na porta ${process.env.SERVER_PORT};`);
+// Inicializa o banco e inicia o servidor
+initializeDatabase()
+    .then(() => {
+        app.listen(process.env.SERVER_PORT, () => {
+            console.log(`Servidor rodando na porta ${process.env.SERVER_PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.error('Erro ao inicializar o banco de dados:', err);
     });
-}).catch(err => {
-    console.error("Erro ao inicializar o banco de dados:", err);
-});
