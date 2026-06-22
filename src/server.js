1ce2789cf18c829ad5e7ebae17aedc66b5fb5ce3
ANDRÉ FERNANDES ANTUNES DA SILVA <andre.silva328@senaisp.edu.br>
2026-06-22 12:34:52 -0300

    decimo commit

--- src/server.js
+++ src/server.js
@@ -1,38 +1,40 @@
-import 'dotenv/config';
+import 'dotenv/config'
 import routes from "./routes/routes.js";
 import express from "express";
 import cors from 'cors';
 import path from "path";
 import { fileURLToPath } from "url";
+import { initializeDatabase } from './configs/Database.js';
 
 const app = express();
 
-app.use(cors());
+app.use(cors())
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 // Caminho completo do arquivo atual
 const __filename = fileURLToPath(import.meta.url);
-
 // Pasta do arquivo atual
 const __dirname = path.dirname(__filename);
 
-// Arquivos estáticos
+// aponta pra uploads (raiz) e mantém /uploads na URL
 app.use(
   "/uploads",
-  express.static(path.resolve(__dirname, "uploads"))
+  express.static(path.resolve("uploads"))
 );
+app.use('/' ,routes);
 
-// Rotas
-app.use('/', routes);
+const PORT = process.env.SERVER_PORT || 8080;
 
-// Porta para deploy ou ambiente local
-const PORT = process.env.PORT || process.env.SERVER_PORT || 8080;
+initializeDatabase().then(
+    ()=>{
+        app.listen(PORT, '0.0.0.0' ,  ()=>{
+            console.log(`Servidor rodando em: http://localhost:${8080}`)
+        })
 
-// Inicialização do servidor
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Servidor rodando na porta: ${PORT}`);
-});
+    }
+).catch(err=>{
+    console.error("Erro ao inicializar o servidor", err)
+})
 
-console.log(`DB_HOST conectado: ${process.env.DB_HOST}`);
+console.log(process.env.DB_HOST);
