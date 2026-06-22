9139174978de88472aab6566f164272462610100
ANDRÉ FERNANDES ANTUNES DA SILVA <andre.silva328@senaisp.edu.br>
2026-06-22 10:34:07 -0300

    Adicionei a port

--- src/server.js
+++ src/server.js
@@ -1,37 +1,23 @@
-import 'dotenv/config'
-import routes from "./routes/routes.js";
-import express from "express";
+import express from 'express';
+import routes from './routes/router.js';
+import { initializeDatabase } from './configs/Database.js';
 import cors from 'cors';
-import path from "path";
-import { fileURLToPath } from "url";
+import path from 'path';
 
 const app = express();
 
-app.use(
-    "uploads/images",
-    express.static("uploads/images")
-)
-
-
-app.use(cors())
+app.use(express.static(path.join(process.cwd(), 'public')));
+app.use(cors());
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-// Caminho completo do arquivo atual
-const __filename = fileURLToPath(import.meta.url);
-// Pasta do arquivo atual
-const __dirname = path.dirname(__filename);
+app.use('/', routes);
 
-// aponta pra uploads (raiz) e mantém /uploads na URL
-app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
-app.use('/' ,routes);
-
-const PORT = process.env.SERVER_PORT || 3000;
-
-initializeDatabase().then(() => {
-    app.listen(process.env.SERVER_PORT, () => {
-        console.log(`Servidor rodando na porta ${process.env.SERVER_PORT}`);
+initializeDatabase()
+    .then(() => {
+        app.listen(process.env.SERVER_PORT, () => {
+            console.log(`Servidor rodando na porta ${process.env.SERVER_PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.error('Erro ao inicializar o banco de dados:', err);
     });
-}).catch(err => {
-    console.error("Erro ao inicializar o banco de dados:", err);
-});
